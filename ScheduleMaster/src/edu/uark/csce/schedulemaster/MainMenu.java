a3cdbbb857ab04b00c22bc63a165a2d62f36ab3e
wbouillo <william.bouillon@gmail.com>
2013-10-06 23:12:21 -0500

    Test
    Trying to get this to work on my computer...

---
+++ ScheduleMaster/src/edu/uark/csce/schedulemaster/MainMenu.java
@@ -0,0 +1,47 @@
+package edu.uark.csce.schedulemaster;
+
+
+import com.example.schedulemaster.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.view.Menu;
+import android.view.View;
+
+public class MainMenu extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_menu);
+	}
+  // Test comment
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main_menu, menu);
+		return true;
+	}
+
+	// Just a simple popup for now, will need to be New Event Item Wizard
+	public void CreateNewEventItem(View view)
+	{
+		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+		
+		alertDialogBuilder
+			.setTitle("Temp Message")
+			.setMessage("Insert New Event Item Wizard fragment here.")
+			.setCancelable(false)
+			.setPositiveButton("OK",new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog,int id) {
+					// If this button is closed, close the dialog
+					dialog.cancel();
+				}
+			  });
+
+		AlertDialog alertDialog = alertDialogBuilder.create();
+		alertDialog.show();
+	}
+}
